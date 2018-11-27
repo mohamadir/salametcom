e471d3075fef9ed963fda3e5bd71f604dc249650
mohamadir <mohamdib@gmail.com>
2018-11-27 15:51:10 +0200

    push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel  ASDFSDFSDF
                 </div>
 
                 <div class="links">
