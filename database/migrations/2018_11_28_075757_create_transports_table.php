c7bbe0613f24b24cf624b8cc70c8a0ac60308dac
mohamadir <mohamdib@gmail.com>
2018-11-28 11:48:11 +0200

    finish making the tables

--- database/migrations/2018_11_28_075757_create_transports_table.php
+++ database/migrations/2018_11_28_075757_create_transports_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTransportsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,29 +13,24 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transports', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->integer('phone');
-            $table->string('position');
-            $table->boolean('is_admin');
-            $table->string('area');
-            $table->string('password');
+            $table->string('from');
+            $table->string('to');
+            $table->integer('people');
+            $table->enum('driver',['volunteer','taxi']);
+            $table->integer('price_share');
             $table->timestamps();
         });
     }
-    
 
     /**
      * Reverse the migrations.
      *
      * @return void
-     * 
-     * 
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transports');
     }
 }
--- database/migrations/2018_11_28_080605_create_helps_table.php
+++ database/migrations/2018_11_28_080605_create_helps_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateHelpsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,29 +13,24 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('helps', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->integer('phone');
-            $table->string('position');
-            $table->boolean('is_admin');
-            $table->string('area');
-            $table->string('password');
+            $table->integer('price')->nullable();
+            $table->string('patient')->nullable();
+            $table->string('hospital')->nullable();
+            $table->string('help_type');
+            $table->string('asked_from');
             $table->timestamps();
         });
     }
-    
 
     /**
      * Reverse the migrations.
      *
      * @return void
-     * 
-     * 
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('helps');
     }
 }
