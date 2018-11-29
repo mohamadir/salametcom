598a883e64124643b54b98cb4a29575021496e26
mohamadir <mohamdib@gmail.com>
2018-11-29 16:31:34 +0200

    add meta data

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{csrf_token()}}" >
+    <meta name="description" content="جمعية سلامتكم, رغم الالم يوجد الم!">
+    <meta property="og:title"  content="جمعية سلامتكم, رغم الالم يوجد الم!"/>
+    <meta property="og:url" content="http://mohamdibrahem.com" />
+
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
