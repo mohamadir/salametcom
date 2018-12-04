70c05830ece9f27d633240dcf161df29909cd3d6
mohamadir <mohamdib@gmail.com>
2018-12-04 15:26:12 +0200

    add graph

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="جمعية سلامتكم, رغم الالم يوجد الم!">
     <meta property="og:title"  content="جمعية سلامتكم, رغم الالم يوجد الم!"/>
     <meta property="og:url" content="http://mohamdibrahem.com" />
-
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0, user-scalable=no">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link href="https://unpkg.com/gijgo@1.9.11/css/gijgo.min.css" rel="stylesheet" type="text/css" />
