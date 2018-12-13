42a5c47a8a888bb3468bf806b3df74ca1a38df2c
mohamadir <mohamdib@gmail.com>
2018-12-13 12:49:24 +0200

    push

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
         // $this->call(UsersTableSeeder::class);
-        DB::table('users')->truncate();
+        /*       DB::table('users')->truncate();
         DB::table('transports')->truncate();
         DB::table('tools')->truncate();
         DB::table('helps')->truncate();
@@ -21,22 +19,22 @@ class DatabaseSeeder extends Seeder
         factory(App\Transport::class,40)->create();
         factory(App\Tool::class,25)->create();
         factory(App\Help::class,29)->create();
-        factory(App\Donate::class,12)->create();
+        factory(App\Donate::class,12)->create(); */
         DB::table('users')->insert([
             'name' => 'محمد ابراهيم',
-            'is_admin' =>  1,
-            'email' => 'mohamdib@gmail.com' ,
-            'password' => '123456' ,
+            'is_admin' => 1,
+            'email' => 'mohamdib@gmail.com',
+            'password' => '123456',
             'phone' => '0509840407',
-            'area' => 'ابوغوش' 
+            'area' => 'ابوغوش',
         ]);
         DB::table('users')->insert([
             'name' => 'يعقوب ابراهيم',
-            'is_admin' =>  1,
-            'email' => 'yaqob@gmail.com' ,
-            'password' => '123456' ,
+            'is_admin' => 1,
+            'email' => 'yaqob@gmail.com',
+            'password' => '123456',
             'phone' => '0545444704',
-            'area' => 'ابوغوش' 
+            'area' => 'ابوغوش',
         ]);
     }
 }
